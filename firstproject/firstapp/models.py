488228ec2a02fa0a85589d3430d12f28c56a660d
Menna Magdy <mennamagdyx@gmail.com>
2024-09-09 13:56:22 +0300

    Adding Database Records

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -8,3 +8,8 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+    #string representations of the tours
+    def __str__(self):
+        return (f"ID {self.id}: FROM {self.origin_country}, TO {self.destination_country}, NIGHTS: {self.number_of_nights}, PRICE: {self.price}")
+        #id added only when instance is saved to the db
